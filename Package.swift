f4d65b440a93e590a1ae059e6292def724d0c574
Dmitry Gnatuk <>
2025-07-03 00:51:08 +0300

    Removed SPM error

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CountriesFlags",
-            exclude: ["CountriesFlagsExample"],
-            dependencies: []),
+            dependencies: [],
+            exclude: ["CountriesFlagsExample"]),
         .testTarget(
             name: "CountriesFlagsTests",
             dependencies: ["CountriesFlags"]),
